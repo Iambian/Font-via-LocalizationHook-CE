4cdb1ce12085d2ceaabf51730a3b4e9cb832fbc8
rawrs <iambian@gmail.com>
2020-09-30 12:51:28 -0400

    small fonts. also, can we rename this project now?

--- tools/fontpacker/packer.py
+++ tools/fontpacker/packer.py
@@ -27,53 +27,60 @@ def tisource(img,glyphid):
             s += t + "\n"
     return s
 def smallfontsource(img,glyphid):
+    li = [0]*12*2
+    w,h = img.size
+    if w>16: w = 16
+    if h>12: h = 12
+    for y in range(h):
+        for x in range(w):
+            if not img.getpixel((x,y)):
+                ofs = x//8
+                li[2*y+ofs] = li[2*y+ofs] | (1 << abs(7-x%8))
+    s = '.db '+str(img.size[0]+1)+'             ;id '+glyphid+" ["+str(ord(glyphid))+"]\n"
+    w = w+1  #Enforce spacing
+    for v1,v2 in zip(li[0::2],li[1::2]):
+        if w>8:
+            t = '.db %'+format(v1,"08b")+",%"+format(v2,"08b")
+        else:
+            t = '.db %'+format(v1,"08b")
+        s += t + "\n"
+    if w<9:
+        s += ".db 0,0,0,0,0,0\n.db 0,0,0,0,0,0\n"
+    return s
     pass
-#Could also use mode "L" for 8bpp gs. Change bg to 255 if you do this.
-img = Image.new("1",(1024,40),1)    #wide image for large string, bg white
-draw = ImageDraw.Draw(img)
-
-if len(sys.argv) > 1:   arg1 = sys.argv[1]  #Input filename
-if len(sys.argv) > 2:   arg2 = sys.argv[2]
-if len(sys.argv) > 3:   arg3 = sys.argv[3]
-
-'''DEBUG'''
-arg1 = "comicbd.ttf"
-arg1 = "klingon2.ttf"
-arg2 = 14
-'''ENDDEBUG'''
-
-if arg1:    fontname = arg1
-else:       quit("Input font filename required")
-if arg2:    fontsize = int(arg2)
-else:       fontsize = 12
-
-ext = os.path.splitext(arg1)[1]
-if ext in (".ttf",".otf"):    func = ImageFont.truetype
-else:                       func = ImageFont.load
-
-font = func(arg1,fontsize)
-thing = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-draw.text((0,0),thing,font=font)
-mainoffset = ImageOps.invert(img.convert("L")).getbbox()
-cimg = img.crop(mainoffset)
-#cimg.show()
-objarr = []
-f = open("output.txt","w")
-print(mainoffset)
-for idx,i in enumerate(thing):
-    timg = Image.new("1",(30,30),1)    #wide image for large string, bg white
-    tdraw = ImageDraw.Draw(timg)
-    tdraw.text((0,0),i,font=font)
-    suboffset = ImageOps.invert(timg.convert("L")).getbbox()
-    if suboffset:
-        offset = (suboffset[0],mainoffset[1],suboffset[2],suboffset[3])
-        ttimg = timg.crop(offset)
-    else:
-        ttimg = timg
-    f.write(tisource(ttimg,i))
-f.close()
-print("File output")    
     
-
+    
+def packit(fontname="klingon2.ttf",fontsize=14,resource=None,outputname="output.txt",packfunc=None):
+    img = Image.new("1",(1024,40),1)    #wide image for large string, bg white
+    draw = ImageDraw.Draw(img)
+    if os.path.splitext(fontname)[1] in (".ttf",".otf"):
+            func = ImageFont.truetype
+    else:   func = ImageFont.load
+    encodestring = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    font = func(fontname,fontsize)
+    draw.text((0,0),encodestring,font=font)
+    mofst = list(ImageOps.invert(img.convert("L",colors=2)).getbbox())
+    if packfunc==smallfontsource:
+        if mofst[1] > 2:    mofst[1] -= 2
+        else:               mofst[1] =  0
+    f = open(outputname,"w")
+    for idx,i in enumerate(encodestring):
+        timg = Image.new("1",(30,30),1)    #wide image for large string, bg white
+        tdraw = ImageDraw.Draw(timg)
+        tdraw.text((0,0),i,font=font)
+        sofst = ImageOps.invert(timg.convert("L",colors=2)).getbbox()
+        if sofst:
+            offset = (sofst[0],mofst[1],sofst[2],sofst[3])
+            ttimg = timg.crop(offset)
+        else:
+            ttimg = timg
+        f.write(packfunc(ttimg,i))
+    f.close()
+    
+    
+    
+#Program starts here:    
+packit("klingon2.ttf",14,None,"output.txt",tisource)
+packit("klingon2.ttf",10,None,"output2.txt",smallfontsource)
 
 
